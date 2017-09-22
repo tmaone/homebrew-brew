022225696a679a0825d05532573ef866c00df44b
tmaone <marios.daoutis@gmail.com>
2017-09-22 20:42:17 +0200

    add fomulae

---
+++ Formula/objconv.rb
@@ -0,0 +1,23 @@
+class Objconv < Formula
+  desc "Convert, modify, dump, and disassemble object files"
+  homepage "http://www.agner.org/optimize/"
+  head "http://www.agner.org/optimize/objconv.zip"
+
+  def install
+    system "unzip", "source.zip"
+    system "/bin/bash", "build.sh"
+    bin.install "objconv"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      const char* my_string = "A test string";
+      int main(void) { return 0; }
+    EOS
+    system ENV.cc, "test.c", "-o", "test"
+    assert (testpath/"test").exist?, "#{ENV.cc} output failed"
+
+    system "#{bin}/objconv", "-fasm", "test"
+    assert (testpath/"test.asm").exist?, "test.asm should exist"
+  end
+end
