bc59cd770ee6590854aa825e539f4d594ea5d280
tmaone <marios.daoutis@gmail.com>
2017-11-24 12:39:42 +0100

    formulae update

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -1,4 +1,4 @@
-class Mfish < Formula
+class fish < Formula
 	desc "User-friendly command-line shell for UNIX-like operating systems"
 	homepage "https://fishshell.com"
 
@@ -15,6 +15,7 @@ class Mfish < Formula
 	depends_on "homebrew/core/ncurses" => :build
 	depends_on "cmake" => :build
 	depends_on "pkg-config" => :build
+  depends_on "gettext" => :build
 	depends_on "pcre2"
 	depends_on "homebrew/core/ncurses"
 	depends_on :python3
@@ -24,8 +25,6 @@ class Mfish < Formula
 	needs :cxx11
 
 	def install
-
-		# ENV.extend(Stdenv)
 
 		ENV["CC"] = "/usr/local/opt/llvm/bin/clang"
 		ENV["CXX"] = "/usr/local/opt/llvm/bin/clang++"
@@ -39,6 +38,7 @@ class Mfish < Formula
 			-G Ninja
 			-DCMAKE_OSX_ARCHITECTURES=x86_64
 			-DCMAKE_BUILD_TYPE=Release
+      -DWITH_GETTEXT=OFF
 			-DCMAKE_INSTALL_PREFIX=#{prefix}
 			-DCMAKE_AR=/usr/local/opt/llvm/bin/llvm-ar
 			-DCMAKE_RANLIB=/usr/local/opt/llvm/bin/llvm-ranlib
@@ -52,33 +52,7 @@ class Mfish < Formula
 			system "ninja"
 			system "ninja", "install"
     end
-		# system "autoreconf", "--no-recursive" if build.head?
-		# In Homebrew's 'superenv' sed's path will be incompatible, so
-		# the correct path is passed into configure here.
-		# args = %W[
-		# 	--prefix=#{prefix}
-		# 	--with-extra-functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d
-		# 	--with-extra-completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d
-		# 	--with-extra-confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d
-		# 	CC=/usr/local/opt/llvm/bin/clang
-		# 	CXX=/usr/local/opt/llvm/bin/clang++
-		# 	LD=/usr/local/opt/llvm/bin/ld.ldd
-		# 	SED=/usr/bin/sed
-		# ]
-    #
-		# system "./configure", *args
-		# system "make", "install"
-	end
 
-	def caveats; <<~EOS
-		You will need to add:
-			#{HOMEBREW_PREFIX}/bin/fish
-		to /etc/shells.
-
-		Then run:
-			chsh -s #{HOMEBREW_PREFIX}/bin/fish
-		to make fish your default shell.
-		EOS
 	end
 
 	def post_install
---
+++ Formula/shairport-sync.rb
@@ -0,0 +1,84 @@
+class ShairportSync < Formula
+  desc "AirTunes emulator that adds multi-room capability"
+  homepage "https://github.com/mikebrady/shairport-sync"
+  url "https://github.com/mikebrady/shairport-sync/archive/3.1.4.tar.gz"
+  sha256 "4c5a2ab40ef49896f5b6e59b20df4f621ebce47ee64d8571336f59820ae66379"
+  head "https://github.com/mikebrady/shairport-sync.git", :branch => "development"
+
+  bottle do
+    sha256 "6a9a583ac9762accefd763a1da03780b1379dbf195016186543d341baf78151b" => :high_sierra
+    sha256 "7295bb44ed4cb087e4ab07d1b146b955982e93e099320bfefb671f2fbeeddfcc" => :sierra
+    sha256 "3ffa9e0965500ecfd2a673d99030af823a8b9b55df98b84b634d049150178466" => :el_capitan
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "openssl"
+  depends_on "popt"
+  depends_on "libsoxr"
+  depends_on "libao"
+  depends_on "libdaemon"
+  depends_on "libconfig"
+  depends_on "pulseaudio"
+
+	ARGV << "--build-from-source"
+	ARGV << "--env=std"
+	ARGV << "--verbose"
+
+
+  def install
+    system "autoreconf", "-fvi"
+    args = %W[
+      --with-os=darwin
+      --with-ssl=openssl
+      --with-dns_sd
+      --with-ao
+      --with-stdout
+      --with-pipe
+      --with-soxr
+      --with-metadata
+      --with-pkg-config
+      --with-piddir=#{var}/run
+      --sysconfdir=#{etc}/shairport-sync
+      --prefix=#{prefix}
+    ]
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  def post_install
+    (var/"run").mkpath
+  end
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/mpd</string>
+            <string>--no-daemon</string>
+            <string>--verbose</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProcessType</key>
+        <string>Interactive</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    output = shell_output("#{bin}/shairport-sync -V", 1)
+    assert_match "OpenSSL-ao-stdout-pipe-soxr-metadata", output
+  end
+end
